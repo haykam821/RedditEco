3d24c73002bc6d7149c920dc9d838b21b18f3044
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-06-12 09:30:02 -0400

    The work has been done... somewhat

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const yargs = require("yargs");
-const config = require("config.json");
+const config = require("./config.json");
 
 const SnooStream = require("snoostream");
 const Snoowrap = require("snoowrap");
 
-const version = require("package.json").version;
+const version = require("./package.json").version;
 
-const snooWrap = new Snoowrap({
-	username: config.username,
-	password: config.password,
-	clientId: config.clientID,
-	clientSecret: config.clientSecret,
+const bot = new Snoowrap({
+	username: config.auth.username,
+	password: config.auth.password,
+	clientId: config.auth.clientID,
+	clientSecret: config.auth.clientSecret,
 	userAgent: `RedditEco ${version} by haykam821`,
 });
 
-const snooStream = SnooStream(snooWrap);
+yargs.command("docs", "Replies with a link to the docs.", {}, argv => {
+    argv.comment.reply(`A user guide is available on [GitHub](https://github.com/haykam821/RedditEco/blob/master/GUIDE.md) and you can ask questions in r/RedditEco.`);
+});
+
+function isCommand(msg) {
+    return author.name !== bot.username //&& msg.body.split(" ")[0].includes(bot.username);
+}
+
+const snooStream = SnooStream(bot);
+const stream = snooStream.commentStream("RedditEcoTest");
+stream.on("post", post => {
+    if (!isCommand(post)) return;
+    yargs.parse(post.body.split(" ").slice(1), {
+        reddit: bot,
+        comment: post,
+    });
+});
